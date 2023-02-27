664eca704577f4b2920b8a6b756f1c0f381abbd0
Altaf Ali <7231912+altaf-ali@users.noreply.github.com>
2023-02-27 21:59:05 +0000

    checkpoint

--- openstack/terraform-output.tf
+++ openstack/terraform-output.tf
@@ -1,12 +1,20 @@
-output "builder-ip-address" {
-  value = openstack_networking_floatingip_v2.builder-ip.address
-}
-
 resource "local_file" "ansible_inventory" {
-  filename        = "ansible/inventory/openstack.yml"
-  file_permission = "0644"
-  content = templatefile("terraform-inventory.tftpl", {
+  filename        = "ansible/inventory/hosts.yml"
+  content = templatefile("ansible/inventory/hosts.tftpl", {
     private_key = trimsuffix(var.ssh_key, ".pub")
-    builder_ip_address = openstack_networking_floatingip_v2.builder-ip.address
+    build_controller_ip_address = openstack_networking_floatingip_v2.build-controller-ip.address
+    build_nodes = tomap({
+      for i in range(var.build_node_count):
+      "${local.build_nodes[i]}" => element(openstack_networking_floatingip_v2.build-node-ip.*.address, i)
+    })
   })
 }
+
+resource "local_file" "infoblox_list" {
+  filename        = "infoblox/test/list.json"
+  content = templatefile("infoblox/test/list.tftpl", {
+    build_controller_hostname = "${var.prefix}-build-controller.${var.infoblox_zone}"
+    build_node_hostnames = [for i in range(var.build_node_count): "${var.prefix}-${local.build_nodes[i]}.${var.infoblox_zone}"]
+  })
+}
+
